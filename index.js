51b23e204396dc2fc22e202135398b58dc1880b6
DharsDev <dharshanpalani.2006@gmail.com>
2024-11-20 18:03:36 +0530

    Changed the front end host URL with some minor adjustments

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://groupchat-socket-io.vercel.app/",
         methods: ["GET","POST"]
     }
 });
@@ -24,17 +24,17 @@ io.on("connection", (socket) => {
 
     socket.on("join_room", (data) => {
         socket.join(data)
-        console.log(`User with ID: ${socket.id} joined the room: ${data}`)
+        // console.log(`User with ID: ${socket.id} joined the room: ${data}`)
     })
 
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message",data)
 
-        console.log(data)
+        // console.log(data)
     })
     
     socket.on("disconnect", () => {
-        console.log("User disconnected")
+        // console.log("User disconnected")
     })
 })
 
