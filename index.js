c58aa8992b64b379945740ef1b3cec7a62cd3a08
DharsDev <dharshanpalani.2006@gmail.com>
2024-11-25 23:20:58 +0530

    Added user alert whenever a user joins in the backend

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
-const http = require("http")
-const cors = require("cors")
+app.use(cors());
 
-const { Server } = require("socket.io")
-
-
-app.use(cors())
-
-
-const server = http.createServer(app)
-
+const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "https://groupchat-socket-io.vercel.app",
-        methods: ["GET","POST"]
-    }
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User connected : ${socket.id}`)
+  console.log(`User connected : ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data)
-        console.log(`User with ID: ${socket.id} joined the room: ${data}`)
-    })
+  socket.on("join_room", (data) => {
+    const { username, room } = data;
+    socket.join(room);
+    console.log(
+      `User with ID: ${socket.id} (username: ${username}) joined the room: ${room}`
+    );
+    socket.to(room).emit("receive_message", {
+      author: "System",
+      message: `${username} has joined the room`,
+      time: new Date().toLocaleTimeString(),
+    });
+  });
 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message",data)
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log(data);
+  });
 
-        console.log(data)
-    })
-    
-    socket.on("disconnect", () => {
-        console.log("User disconnected")
-    })
-})
-
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 server.listen(3001, () => {
-    console.log("Server running")
-})
+  console.log("Server running on port 3001");
+});
